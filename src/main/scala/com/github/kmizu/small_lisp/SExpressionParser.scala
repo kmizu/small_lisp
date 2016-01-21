dfbfa5d5e6df302685b75f06943384fe075af283
Kota Mizushima <mizukota@gmail.com>
2016-01-21 15:28:42 +0900

    Implement parser partially(except whitespace handling)

--- src/main/scala/com/github/kmizu/small_lisp/SExpressionParser.scala
+++ src/main/scala/com/github/kmizu/small_lisp/SExpressionParser.scala
@@ -12,12 +12,23 @@ object SExpressionParser {
   case class BoolValue(value: Boolean) extends Atom
   case class Identifier(value: String) extends Atom
 
-  val exp: P[SExpression] = P(list | atom)
-  val list: P[SList] = P("(" ~ exp.rep(0) ~ ")").map(p => SList(p.toList))
+
+  val hexDigit      = P( CharIn('0'to'9', 'a'to'f', 'A'to'F') )
+  val unicodeEscape = P( "u" ~ hexDigit ~ hexDigit ~ hexDigit ~ hexDigit )
+  val escape        = P( "\\" ~ (CharIn("\"/\\bfnrt") | unicodeEscape) )
+  val chars         = P(CharsWhile(!"\"\\".contains(_: Char)))
+
+  val expression: P[SExpression] = P(list | atom)
+  val list: P[SList] = P("(" ~ expression.rep(0) ~ ")").map(p => SList(p.toList))
   val atom: P[Atom] = P(integer | double | string | bool | identifier)
-  val integer: P[IntValue] = ???
-  val double: P[DoubleValue] = ???
-  val string: P[StringValue] = ???
-  val bool: P[BoolValue] = ???
-  val identifier: P[Identifier] = ???
+  val integer: P[IntValue] = P(CharIn('0'to'9').rep(1).!.map{v => IntValue(v.toInt)})
+  val double: P[DoubleValue] = P((CharIn('0'to'9').rep(1) ~ "." ~ CharIn('0'to'9').rep(1)).!.map{v => DoubleValue(v.toDouble)})
+  val string: P[StringValue] = P("\"" ~ (chars | escape).rep(0).! ~ "\"").map(StringValue(_))
+  val bool: P[BoolValue] = P(("true".!.map{_ => true} | "false".!.map{_ => false}).map{BoolValue(_)})
+  val identifier: P[Identifier] = P{
+    (identifierFirst ~ (identifierFirst | P(CharIn('0'to'9'))).rep(0)).!.map{Identifier(_)}
+  }
+  val identifierFirst: P[String] = P {
+    CharIn('a' to 'z', 'A' to 'Z', List('_', '-', '+', '*', '/', '#', '$', '&', '!', '?', '%', '|', '=', '^', ':')).!
+  }
 }
